f9f6011b94e03047fc0a8dd0e9fbce2b7575b3a0
Jonas Molsted <jonasmolsted@gmail.com>
2025-02-21 16:38:14 +0100

    pew on button press

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,23 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Shoot : MonoBehaviour
 {
     public LayerMask mask;
 
+    public InputActionProperty shootAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray (transform.position, transform.rotation * Vector3.forward);
-        RaycastHit hitInfo;
-		if (Physics.Raycast (ray, out hitInfo, 300, mask, QueryTriggerInteraction.Ignore)) {
-			Destroy (hitInfo.transform.gameObject);
-		}
+        float shootActivate = shootAction.action.ReadValue<float>();
+
+        if (shootActivate == 1.0f) {
+            print(shootActivate);
+
+            Ray ray = new Ray (transform.position, transform.rotation * Vector3.forward);
+            RaycastHit hitInfo;
+		    if (Physics.Raycast (ray, out hitInfo, 200000, mask, QueryTriggerInteraction.Ignore)) {
+			    Destroy (hitInfo.transform.gameObject);
+		    }
+        }
+
     }
 }
